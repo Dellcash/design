f24ff4e84f660c631fff2968bd8848e7269a2381
unknown <omiddellcash@gmail.com>
2022-04-20 13:52:53 +0430

    header on large screen

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,28 @@ import Navigation from "./components/Navigation.vue";
 <template>
 	<Navigation />
 	<main>
-		<img src="./assets/images/image-hero-mobile.png" alt="" w-full h-full />
-		<div mt-8>
-			<h2 text="center 1.8rem Black">Make remote work</h2>
+		<img
+			src="./assets/images/image-hero-mobile.png"
+			alt=""
+			w-full
+			h-full
+			lg:hidden
+		/>
+		<div mt-8 text-center>
+			<h2 text=" 1.8rem Black">Make remote work</h2>
+			<p text="Gray sm" mt-3 px-5 tracking-tight>
+				Get your team in sync, no matter your location. Streamline processes,
+				create team rituals, and watch productivity soar.
+			</p>
+			<div flex items-center justify-center mt-5>
+				<button text-White bg-Black rounded-xl p-3 px-18px>Learn More</button>
+			</div>
+		</div>
+		<div flex items-center justify-between mt-8 mx-2 mb-5 md:mx-5 md:mt-10>
+			<img src="./assets/images/client-databiz.svg" alt="" w-17 md:w-30 />
+			<img src="./assets/images/client-audiophile.svg" alt="" w-10 md:w-18 />
+			<img src="./assets/images/client-maker.svg" alt="" w-14 md:w-30 />
+			<img src="./assets/images/client-meet.svg" alt="" w-14 md:w-30 />
 		</div>
 	</main>
 </template>
@@ -22,10 +41,15 @@ import Navigation from "./components/Navigation.vue";
 	font-family: "Epilogue", sans-serif;
 }
 
+body {
+	background-color: hsl(0, 0%, 98%);
+}
+
 button {
 	border: none;
 	background-color: transparent;
 	outline: none;
+	cursor: pointer;
 }
 
 li {
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -4,6 +4,7 @@ import Todo from "../assets/images/icon-todo.svg";
 import Calendar from "../assets/images/icon-calendar.svg";
 import Reminders from "../assets/images/icon-reminders.svg";
 import Planning from "../assets/images/icon-planning.svg";
+import { onClickOutside } from "@vueuse/core";
 
 const menu = ref(true);
 
@@ -27,30 +28,27 @@ const features = ref([
 ]);
 const feature = ref(false);
 const company = ref(false);
+const outSide = ref();
+onClickOutside(outSide, () => {
+	feature.value = false;
+	company.value = false;
+});
 </script>
 
 <template>
-	<header p-5>
-		<div flex items-center justify-between>
-			<h2 text-2rem tracking-tighter>snap</h2>
-			<button @click="menu = !menu">
+	<header p-5 lg:p-10 lg:flex lg:items-center lg:justify-between>
+		<div flex items-center justify-between lg:justify-start>
+			<h2 text-2rem tracking-tighter lg:text-3rem>snap</h2>
+
+			<button @click="menu = !menu" lg:hidden>
 				<img v-if="menu" src="../assets/images/icon-menu.svg" alt="" />
 			</button>
-		</div>
-	</header>
-	<div>
-		<div v-if="!menu" bg="#000" h-screen absolute top-0 opacity-50 w-full />
-		<div v-if="!menu" absolute w="[60%]" h-screen bg-white top-0 right-0>
-			<div flex justify-end p-5>
-				<button @click="menu = !menu">
-					<img src="../assets/images/icon-close-menu.svg" alt="" />
-				</button>
-			</div>
-			<div p-5>
-				<ul text="sm Gray">
-					<li mb-3>
+
+			<div ml-13 hidden lg:block>
+				<ul flex text="xl Gray" space-x-13>
+					<li relative>
 						<button @click="feature = !feature">
-							<span text-Gray pr-2>Features</span>
+							<span pr-2 text="xl Gray" hover:text-Black>Features</span>
 							<img
 								v-if="!feature"
 								src="../assets/images/icon-arrow-down.svg"
@@ -58,17 +56,36 @@ const company = ref(false);
 							/>
 							<img v-else src="../assets/images/icon-arrow-up.svg" alt="" />
 						</button>
-						<ul v-if="feature" pl-5 my-5 space-y-5>
-							<li v-for="feature in features" :key="feature" flex items-center>
+						<ul
+							ref="outSide"
+							v-if="feature"
+							text="sm Gray"
+							p-8
+							my-5
+							space-y-2
+							absolute
+							right-0
+							rounded-xl
+							shadow-xl
+							bg="[#fff]"
+						>
+							<li
+								v-for="feature in features"
+								:key="feature"
+								flex
+								items-center
+								cursor-pointer
+								hover:text-Black
+							>
 								<img :src="feature.img" alt="" pr-3 />
 								{{ feature.title }}
 							</li>
 						</ul>
 					</li>
 
-					<li mb-3>
+					<li relative>
 						<button @click="company = !company">
-							<span text-Gray pr-2>Company</span>
+							<span pr-2 text="xl Gray" hover:text-Black>Company</span>
 							<img
 								v-if="!company"
 								src="../assets/images/icon-arrow-down.svg"
@@ -76,17 +93,128 @@ const company = ref(false);
 							/>
 							<img v-else src="../assets/images/icon-arrow-up.svg" alt="" />
 						</button>
-						<ul v-if="company" pl-5 my-5 space-y-5>
+						<ul
+							ref="outSide"
+							v-if="company"
+							p-5
+							my-5
+							space-y-2
+							absolute
+							left-0
+							rounded-xl
+							shadow-xl
+							bg="[#fff]"
+							text="sm Gray"
+						>
 							<li>History</li>
 							<li>Our Team</li>
 							<li>Blog</li>
 						</ul>
 					</li>
 
-					<li mb-3>Careers</li>
-					<li>About</li>
+					<li cursor-pointer hover:text-Black>Careers</li>
+
+					<li cursor-pointer hover:text-Black>About</li>
 				</ul>
 			</div>
 		</div>
-	</div>
+		<div hidden lg:block space-x-5>
+			<button text="base Gray" p-4 hover:text-Black>Login</button>
+			<button
+				text="base White"
+				rounded-5
+				px-5
+				py-3
+				bg-Black
+				hover="bg-transparent border text-Black"
+			>
+				Register
+			</button>
+		</div>
+	</header>
+	<nav>
+		<div>
+			<div v-if="!menu" bg="#000" h-full fixed top-0 opacity-50 w-full />
+			<Transition name="slide">
+				<div
+					v-if="!menu"
+					fixed
+					w="[60%]"
+					md="w-[40%]"
+					h-full
+					bg-white
+					top-0
+					right-0
+				>
+					<div flex justify-end p-5>
+						<button @click="menu = !menu">
+							<img src="../assets/images/icon-close-menu.svg" alt="" />
+						</button>
+					</div>
+					<div p-5>
+						<ul text="sm Gray">
+							<li mb-3>
+								<button @click="feature = !feature">
+									<span text-Gray pr-2>Features</span>
+									<img
+										v-if="!feature"
+										src="../assets/images/icon-arrow-down.svg"
+										alt=""
+									/>
+									<img v-else src="../assets/images/icon-arrow-up.svg" alt="" />
+								</button>
+								<ul v-if="feature" pl-5 my-5 space-y-5>
+									<li
+										v-for="feature in features"
+										:key="feature"
+										flex
+										items-center
+									>
+										<img :src="feature.img" alt="" pr-3 />
+										{{ feature.title }}
+									</li>
+								</ul>
+							</li>
+
+							<li mb-3>
+								<button @click="company = !company">
+									<span text-Gray pr-2>Company</span>
+									<img
+										v-if="!company"
+										src="../assets/images/icon-arrow-down.svg"
+										alt=""
+									/>
+									<img v-else src="../assets/images/icon-arrow-up.svg" alt="" />
+								</button>
+								<ul v-if="company" pl-5 my-5 space-y-5>
+									<li>History</li>
+									<li>Our Team</li>
+									<li>Blog</li>
+								</ul>
+							</li>
+
+							<li mb-3>Careers</li>
+							<li>About</li>
+						</ul>
+					</div>
+				</div>
+			</Transition>
+		</div>
+	</nav>
 </template>
+
+<style>
+.slide-enter-active {
+	transition: all 0.3s ease-out;
+}
+
+.slide-leave-active {
+	transition: all 0.2s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-enter-from,
+.slide-leave-to {
+	transform: translateX(20px);
+	opacity: 0;
+}
+</style>
